3f32c7cc7217921d59702bfcc282cf67eb8fd9a0
Keet <keetsta@icloud.com>
2024-12-29 00:04:07 +0500

    Add files via upload

--- browse.lua
+++ browse.lua
@@ -1,5 +1,16 @@
---[[pod_format="raw",created="2024-12-28 18:54:09",modified="2024-12-28 18:59:19",revision=3]]
--- by keet
+--[[pod_format="raw",created="2024-12-28 18:54:09",modified="2024-12-28 19:03:46",revision=4]]
+--[[	Thanks for using PicoNet Sitebuilder! I look forward
+	to seeing your picosite! In order to make this available
+	on PicoNet Explorer, you'll need to upload this lua file
+	somewhere on the internet that you can get a link to it.
+	Then, you'll want to go to the following thread:
+	
+		https://www.lexaloffle.com/bbs/?tid=140960
+	
+	and post your link based on the format provided there.
+	it's  very possible that this format could change slightly
+	and so I will not be listing that here just yet.
+]]
 
 	p = {
 	title="keet ",
@@ -12,7 +23,7 @@
 	init = function(self,explorer)
 		local page = self
 		add(self.copybuttons,self.g:attach({
-			x=90, y=8, width=16, height=16,
+			x=90.0, y=46.0, width=16, height=16,
 			event = function(self,msg)
 				if(msg.event == "release") then
 					set_clipboard(pod({type="gif", w=16, h=16, frames=8, speed=1, clr=0, imgdata=page.gif3_gif_raw}))
@@ -21,14 +32,14 @@
 			end
 		}))
 		self.button6 = self.g:attach_button({
-			x=3.5, y=119.5, width=67, height=35,
-			label="<< otomir23 ",
+			x=3.5, y=135.5, width=67, height=35,
+			label="\n<< otomir23 ",
 			click=function()
-				explorer:open_page("https://i.otomir23.me/pico/index.lua")
+				explorer:open_page("https://raw.githubusercontent.com/May0san/piconetdemo/main/testpage.lua")
 			end
 		})
 		add(self.copybuttons, self.g:attach({
-			x=113, y=38, width=24, height=24,
+			x=112.0, y=72.0, width=24, height=24,
 			event = function(self,msg)
 				if(msg.event == "release") then
 					set_clipboard("--[[pod,pod_type=\"image\"]]unpod(\""..page.image7_raw.."\")")
@@ -42,15 +53,15 @@
 	end,
 	draw = function(self,explorer)
 		cls(7)
-		print("this is keet's\ncool website!\n ",11,5,30)
-		self.gif3:draw(90,8,16,16,1,0)
-		rect(0,116,291,156,27)
-		print("webring ",130,122,26)
+		print("this is keet's\ncool website!\n ",7.0,42.0,30)
+		self.gif3:draw(90.0,46.0,16,16,1,0)
+		rect(0,132,291,172,27)
+		print("\n\n\n\nwebring ",134.0,102.0,26)
 		palt(0)
 		palt(0, true)
-		sspr(self.image7, 0, 0, 16,16,113,38,24,24)
+		sspr(self.image7, 0, 0, 16,16,112.0,72.0,24,24)
 		palt()
-		print("contact me in\ntelegram - @keetsta\n ",10,37,3)
+		print("contact me in\ntelegram - @keetsta\n ",6.0,68.0,3)
 		
 	end,
 	update = function(self,explorer)
